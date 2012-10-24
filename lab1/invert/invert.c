a0c4211bf8272ad0403bf9801fe1d4aa7976a2cd
Noy Eliahu <eliahu.noy@gmail.com>
2012-10-24 16:59:20 +0200

    added lab1

---
+++ lab1/invert/invert.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+/* count number of words in sentence */
+int main(int argc, char **argv) {
+  int i;
+ 
+  
+  for(i=argc-1; i!=0; --i) {
+    printf("%s", argv[i]);
+ 
+    if(i!=1)
+      printf(" ");
+  }
+  printf("\n");
+ 
+  return 0;
+} 
