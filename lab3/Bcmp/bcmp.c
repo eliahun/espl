4b6b6f414efaafa39523182f1bebf9d98f441f16
Noy Eliahu <eliahu.noy@gmail.com>
2012-11-26 14:43:50 +0200

    in progress

---
+++ lab3/Bcmp/bcmp.c
@@ -0,0 +1,133 @@
+  #include <stdio.h>
+  #include <string.h>
+  #include <stdlib.h>
+  #include <unistd.h>
+
+
+  /* OPTIONS
+	-h  print a summary of options and exit
+	-n  NUMBER  print first NUMBER differences and exit
+	-q  do not print out the differences, exits with code 0
+	if the files are identical, 1 otherwise */
+
+ // struct globalArgs_t {
+ //     int *options;                /* -h option */
+ //     int *number;               /* -n option */
+ //     int *quit;               /* -q option */
+      
+	      
+// } globalArgs;
+ // static const char *optString = "h:n:q";
+ 
+
+  int main(int argc, char **argv) {
+    
+    int opt = 0;
+    int num=0;
+    static const char *optString = "h:n:q";
+    
+     int options=0;                /* -h option */
+     int number=0;               /* -n option */
+     int quit=0;
+    
+    
+      /* Initialize globalArgs before we get to work. */
+    //globalArgs.options =0;     /* false */
+    //globalArgs.number = 0;
+    //globalArgs.quit = 0;
+    
+    
+    
+    
+    FILE *first_file=fopen(argv[argc-2], "r"); 
+    FILE *second_file=fopen(argv[argc-1], "r"); 
+    size_t result;
+    
+    char* first_buffer=NULL;
+    char* second_buffer=NULL;
+    int lSize_1;
+    int lSize_2;
+    
+    fseek (first_file , 0 , SEEK_END);
+    lSize_1 = ftell (first_file);
+    rewind (first_file);
+		    
+    first_buffer = (char*) malloc (sizeof(char)*lSize_1);
+    result = fread (first_buffer,1,lSize_1,first_file);
+      
+    fseek (second_file , 0 , SEEK_END);
+    lSize_2 = ftell (second_file);
+    rewind (second_file);
+		    
+    second_buffer = (char*) malloc (sizeof(char)*lSize_2);
+    result = fread (second_buffer,1,lSize_2,second_file); 
+    
+    opt = getopt( argc, argv, optString );
+	  
+	
+      while( opt != -1 ) {
+	  switch( opt ) {
+	      case 'h':
+		options=1;
+		  break;
+		  
+	      case 'n':
+		number=1;
+		num=atoi(argv[argc-3]);
+		break;
+		  
+	      case 'q':
+		  quit=1;
+		  break;
+		  
+		  
+	      default:
+		  /* You won't actually get here. */
+		  break;
+	  }
+	  opt = getopt( argc, argv, optString );
+      }
+      
+      
+    int j;
+    int counter=0;
+      
+    for (j=0; j<lSize_1 || j<lSize_2; j++){
+	
+      
+	if (first_buffer[j]!= second_buffer[j]){
+	    printf( "#%i: ", j );
+	    printf( "-%i +%i\n", first_buffer[j], second_buffer[j] );
+	    counter++;
+	  }  
+	 if (first_buffer[j]== second_buffer[j]){
+	   printf( "#%i: \n", j );
+	 }
+  
+      }
+      
+      
+      if (options==1){
+	printf( "OPTIONS: \n-h  print a summary of options and exit\n-n NUMBER  print first NUMBER differences and exit\n-q do not print out the differences, exits with code 0\nif the files are identical, 1 otherwise\n"); 
+      }
+      
+      
+      if (number==1){
+	printf( "number of differences %i \n",counter ); 
+	printf( "NUMBER %i \n",num ); 
+      }
+      
+      
+      if (quit==1){
+	return 0;
+      }
+      
+    
+      
+      
+      
+      fclose(first_file);
+      fclose(second_file);
+    
+    return 0;
+  }
